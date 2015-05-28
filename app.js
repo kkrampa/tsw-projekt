d8eb0de04b9c67b94c664f461634c290fde69b47
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-28 21:44:10 +0200

    Switched template enginge to swig

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+var swig  = require('swig');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
