329ec9498504ea7a6b924a43a562bb84e77acba0
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-29 23:59:11 +0200

    Fixed jshint errors

--- public/javascripts/app/game/controllers/boardController.js
+++ public/javascripts/app/game/controllers/boardController.js
@@ -2,9 +2,9 @@
     "use strict";
     var gameModule = angular.module('game', ['ngDragDrop']);
     gameModule.controller('boardCtrl', function($scope) {
-		var Cell = function(color) {
+		var Cell = function(cellColor) {
             var self = this;
-			var color = color || 'white';
+			var color = cellColor || 'white';
             this.letter = null;
             
             this.getColor = function() {
@@ -12,7 +12,7 @@
                     return 'antiquewhite';
                 }
                 return color;
-            }
+            };
 		};
 		
 		var Letter = function(character) {
@@ -22,16 +22,16 @@
         var polishCharacters = [
             'A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','R', 
             'S','Ś','T','U','W','Y','Z','Ź','Ż'
-	    ]
+	    ];
         
         $scope.letters = polishCharacters.map(function(el) {
             return new Letter(el);
         });
         
         $scope.onDrop = function(event, ui, letter, $index, $parent) {
-            var letter = ui.draggable.text();
+            var draggableLetter = ui.draggable.text();
             ui.draggable.remove();
-            $scope.board[$parent][$index].letter = letter;
+            $scope.board[$parent][$index].letter = draggableLetter;
         };
         		
 		$scope.board = [
