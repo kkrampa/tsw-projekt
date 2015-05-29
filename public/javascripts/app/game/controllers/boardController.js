b099aa353f60827b5230e76c0a6cd91095a8e50a
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-29 23:14:39 +0200

    Added basic drag and drop functionality

--- public/javascripts/app/game/controllers/boardController.js
+++ public/javascripts/app/game/controllers/boardController.js
@@ -1,9 +1,10 @@
 (function() {
     "use strict";
-    var gameModule = angular.module('game', []);
+    var gameModule = angular.module('game', ['ngDragDrop']);
     gameModule.controller('boardCtrl', function($scope) {
 		var Cell = function(color) {
 			this.color = color || 'white';
+            this.letter = null;
 		};
 		
 		var Letter = function(character) {
@@ -14,8 +15,17 @@
             'A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','R', 
             'S','Ś','T','U','W','Y','Z','Ź','Ż'
 	    ]
-	    var letters = [];
-		
+        
+        $scope.letters = polishCharacters.map(function(el) {
+            return new Letter(el);
+        });
+        
+        $scope.onDrop = function(event, ui, letter, $index, $parent) {
+            var letter = ui.draggable.text();
+            ui.draggable.remove();
+            $scope.board[$parent][$index].letter = letter;
+        };
+        		
 		$scope.board = [
 			[
 				new Cell('red'), new Cell(), new Cell(), new Cell('lightblue'), new Cell(), 
