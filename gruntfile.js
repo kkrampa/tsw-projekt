dcb5715c1800f35a53e80626d00c72abc4f2b587
Kamil Krampa <kkrampa@soldevelo.com>
2015-06-06 22:03:15 +0200

    Fixed grunt and travis build

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,26 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      myFiles: ['./public/javascripts/*.js', '*.js', './routes/*.js']
-    }
-  });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            myFiles: ['./public/javascripts/*.js', '*.js', './routes/*.js']
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('mocha', 'mochaTest');
+
 };
