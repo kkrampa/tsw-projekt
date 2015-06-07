af04ae72cccd4480288331520cdbdda98e9e6c39
Kamil Krampa <kkrampa@soldevelo.com>
2015-06-07 13:52:15 +0200

    Changed tests

--- server.js
+++ server.js
@@ -3,10 +3,33 @@ module.exports = function(server) {
     var io = require("socket.io")(server);
     this.active = false;
     this.users = [];
+    var rooms = [];
+    var userRoom = {};
+    
     io.sockets.on("connection", function (socket) {
         self.users.push(socket);
-        if (self.users === 2) {
+        if (self.users.length === 2) {
             self.active = true;
         }
+        
+        socket.on("createRoom", function(room) {
+            socket.join(room);
+            userRoom[socket.id] = room;
+            rooms.push(room);
+        });
+    
+        socket.on("rooms", function() {
+            socket.emit('rooms', rooms);
+        });
+        
+        socket.on("disconnect", function(data) {
+            socket.leave(userRoom[socket.id]);
+            for (var i = 0; i < self.users.length; i++) {
+                if (self.users[i].id === socket.id) {
+                    self.users.splice(i, 1);
+                    break;
+                }
+            }
+        });
     });
 };
--- test/test-socket-io-server.js
+++ test/test-socket-io-server.js
@@ -1,6 +1,8 @@
-var assert = require('chai').assert;
+/*jshint node: true */
+
 var io = require('socket.io-client');
 var http = require('http');
+var should = require('should');
 
 var socketURL = 'http://localhost:3000';
 
@@ -17,22 +19,61 @@ describe("Game server", function() {
 
     var client = null;
     var client2 = null;
-    it('should connect one user to socket', function() {
+    it('should connect one user to socket', function(done) {
         client = io.connect(socketURL, options);
         client.on('connect', function(){
-            assert.equal(server.users.length, 1);
+            server.users.length.should.equal(1);
+            client.disconnect();
+            done();
         });
-
     });
     
-    it('should return that game is not active', function() {
-        assert.isFalse(server.active);
+    it('should return that game is not active', function(done) {
+        client = io.connect(socketURL, options);
+        client.on('connect', function() {
+            server.active.should.be.false;
+            done();
+        });
     });
     
-    it('should return that game is active', function() {
+    it('should return that game is active', function(done) {
+        client = io.connect(socketURL, options);
+        client.on('connect', function() {
+            client2 = io.connect(socketURL, options);
+            client2.on('connect', function() {
+                server.active.should.be.true;
+                client.disconnect();
+                client2.disconnect();
+                done();
+            });
+        });
+    });
+    
+    it('should return no rooms', function(done) {
         client2 = io.connect(socketURL, options);
         client2.on('connect', function() {
-            assert.isTrue(server.active);
+            client2.on('rooms', function(rooms) {
+                rooms.length.should.equal(0);
+                done();
+            });
+        
+            client2.emit('rooms');
         });
     });
+    
+    it('should create room', function(done) {
+        client = io.connect(socketURL, options);
+        client.on('connect', function() {
+            client.emit("createRoom", "test");
+            client.emit("rooms");
+            client.on('rooms', function(rooms) {
+                rooms.length.should.equal(1);
+                var room = rooms[0];
+                room.should.equal("test");
+                done();
+            });
+        });
+    });
+    
+    
 });
